8330d953eddc5ad0cf9c5c7e2512fabac3b1938d
Steffen568 <sgonzalez568@gmail.com>
2022-04-25 11:37:54 -0500

    completed server setup/created route directory

---
+++ server.js
@@ -0,0 +1,20 @@
+// import xpress
+const express = require('express')
+const app = express()
+
+// create port
+const PORT = process.env.PORT || 4001
+
+// read incoming json data
+app.use(express.urlencoded({ extend: true }))
+app.use(express.json())
+app.use(express.static('public'))
+
+// create paths within server
+require('./routes/apiRoutes')(app)
+require('./routes/htmlRoutes')(app)
+
+
+app.listen(PORT, () => {
+    console.log(`App listening on PORT ${PORT}`)
+})
